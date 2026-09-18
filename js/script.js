24f1761865d0d3ab0e9d2a222bc680915c9cf9d9
alelinainf-eng <alelina.inf@gmail.com>
2026-09-18 13:35:24 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,38 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+const warrantyForm = document.querySelector(".warranty-form");
+
+if (warrantyForm) {
+
+    warrantyForm.addEventListener("submit", function (event) {
+
+        event.preventDefault();
+
+        const serial = document.querySelector("#serial").value.trim();
+        const code = document.querySelector("#national").value.trim();
+        const result = document.querySelector("#result");
+
+        if (!serial || !code) {
+
+            result.classList.add("active");
+            result.innerHTML = `
+                <span style="color:#e5c45a;">
+                    لطفاً تمام اطلاعات را وارد کنید.
+                </span>
+            `;
+
+            return;
+        }
+
+        result.classList.add("active");
+
+        result.innerHTML = `
+            <strong style="color:#e5c45a;">
+                در حال بررسی اطلاعات...
+            </strong>
+        `;
+
+    });
+
+}
